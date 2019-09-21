59b4de34e3dfb5cd793e6565ccfb478deb2c3c21
KimJunKi0324 <rimeilo324@naver.com>
2019-09-21 16:55:55 +0900

    19_09_21_React_PropTypes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
 
+function Food({name, picture, rating}){  /* props.fav == {fav} */
+  return <div>{name}, {picture}, {rating} </div>
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: "kimchi",
+    image: "no",
+    rating: 5
+  },
+  {
+    id: 2,
+    name: "chchch",
+    image: "bobobo",
+    rating:4.9
+  }
+]
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ( 
+  <div>
+  {foodILike.map(dish => (
+    <Food key= {dish.id} name = {dish.name} picture = {dish.image} rating = {dish.rating}/>
+  ))}
+  </div>
+   ) /*return하고 줄 바꾸면 외않됨?*/
 }
 
 export default App;
